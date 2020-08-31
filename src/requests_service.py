eef5a0d9950496b1b6b97bc3965959f6dadc84b2
joao reis <1160600@isep.ipp.pt>
2020-08-31 18:29:59 +0100

    comment fix

--- src/requests_service.py
+++ src/requests_service.py
@@ -6,10 +6,11 @@ def request_skin_price(weapon, skin, weapon_cond):
     payload = {'currency': '3', 'appid': '730', 'market_hash_name': weapon_hash}
     url = "https://steamcommunity.com/market/priceoverview/"
     #url ='https://steamcommunity.com/market/priceoverview/?currency=3&appid=730&market_hash_name=P90%20%7C%20Asiimov%20%28Factory%20New%29'  url exemplo
-    params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)  #usado para o encoding, ou seja, caracteres como espaços sao mudados para codigos especificos
-                                                                            #espaço - %20
-                                                                            #( - %28
-                                                                            #) - %29 etc
+    params = urllib.parse.urlencode(payload, quote_via=urllib.parse.quote)
+    #usado para o encoding, ou seja, caracteres como espaços sao mudados para codigos especificos
+    #espaço - %20
+    #( - %28
+    #) - %29 etc
     return requests.get(url, params=params)
 
 def generate_weapon_hash(weapon, skin, weapon_cond):
